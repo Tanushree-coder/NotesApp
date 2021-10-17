aa33fccdafc7130d436f48a6a4b7a22e0e2a3f76
Tanushree-coder <tanushree950444@gmail.com>
2021-10-17 20:19:27 +0530

    added layout

--- app/src/main/java/com/example/notesapplication/MainActivity.java
+++ app/src/main/java/com/example/notesapplication/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.notesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn_add, btn_reset;
+    RecyclerView recyclerView;
+
+    List<MainData> dataList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.edit_text);
+        btn_add=findViewById(R.id.add);
+        btn_reset=findViewById(R.id.reset);
+        recyclerView=findViewById(R.id.recyclerView);
+
+
     }
 }
